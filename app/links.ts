c3374417dedb614f288ba62734373386cc873a75
Peiju Lin <linkevin422@gmail.com>
2026-04-20 09:05:10 +0800

    vibe coding was missing

--- app/links.ts
+++ app/links.ts
@@ -5,46 +5,193 @@
 // https://yourdomain.com/[slug]?utm_source=[source]&utm_campaign=[campaign]
 
 const LINKS: Record<string, string> = {
-    // =====================
-    // THINKNODE
-    // =====================
-  
-    "thinknode": "https://shopee.tw/product/474186812/47608007722/",
-  
-    // YouTube
-    "thinknode-yt": "https://shopee.tw/product/474186812/47608007722/",
-    "thinknode-yts": "https://shopee.tw/product/474186812/47608007722/",
-  
-    // Instagram
-    "thinknode-ig-bio": "https://shopee.tw/product/474186812/47608007722/",
-    "thinknode-ig-story": "https://shopee.tw/product/474186812/47608007722/",
-  
-    // Email
-    "thinknode-email": "https://shopee.tw/product/474186812/47608007722/",
-  
-    // Website
-    "thinknode-site": "https://shopee.tw/product/474186812/47608007722/",
-  
-    // Threads
-    "thinknode-threads": "https://shopee.tw/product/474186812/47608007722/",
-  
-    // =====================
-    // NEXT BOOK (example)
-    // =====================
-  
-    // "vibebook": "https://...",
-    // "vibebook-yt": "https://...",
-    // "vibebook-yt1": "https://...",
-    // "vibebook-ig": "https://...",
-    // "vibebook-email": "https://...",
-    // "vibebook-site": "https://...",
-  
-  
-    // =====================
-    // OTHER / TESTING
-    // =====================
-  
-    // "test": "https://...",
-  };
-  
+  // =====================
+  // 連結設計
+  // =====================
+
+  "thinknode": "https://shopee.tw/product/474186812/47608007722/",
+
+  // YouTube
+  "thinknode-yt": "https://shopee.tw/product/474186812/47608007722/",
+  "thinknode-yts": "https://shopee.tw/product/474186812/47608007722/",
+
+  // Instagram
+  "thinknode-ig-bio": "https://shopee.tw/product/474186812/47608007722/",
+  "thinknode-ig-story": "https://shopee.tw/product/474186812/47608007722/",
+
+  // Email
+  "thinknode-email": "https://shopee.tw/product/474186812/47608007722/",
+
+  // Website
+  "thinknode-site": "https://shopee.tw/product/474186812/47608007722/",
+
+  // Threads
+  "thinknode-threads": "https://shopee.tw/product/474186812/47608007722/",
+
+
+  // =====================
+  // 音樂製作人 X FL Studio
+  // =====================
+
+  "flstudioproducer": "https://shopee.tw/product/474186812/27465576743/",
+
+  // YouTube
+  "flstudioproducer-yt": "https://shopee.tw/product/474186812/27465576743/",
+  "flstudioproducer-yts": "https://shopee.tw/product/474186812/27465576743/",
+
+  // Instagram
+  "flstudioproducer-ig-bio": "https://shopee.tw/product/474186812/27465576743/",
+  "flstudioproducer-ig-story": "https://shopee.tw/product/474186812/27465576743/",
+
+  // Email
+  "flstudioproducer-email": "https://shopee.tw/product/474186812/27465576743/",
+
+  // Website
+  "flstudioproducer-site": "https://shopee.tw/product/474186812/27465576743/",
+
+  // Threads
+  "flstudioproducer-threads": "https://shopee.tw/product/474186812/27465576743/",
+
+
+  // =====================
+  // FL Studio 嘻哈製作人
+  // =====================
+
+  "flstudiohiphopproducer": "https://shopee.tw/product/474186812/25292961904/",
+
+  // YouTube
+  "flstudiohiphopproducer-yt": "https://shopee.tw/product/474186812/25292961904/",
+  "flstudiohiphopproducer-yts": "https://shopee.tw/product/474186812/25292961904/",
+
+  // Instagram
+  "flstudiohiphopproducer-ig-bio": "https://shopee.tw/product/474186812/25292961904/",
+  "flstudiohiphopproducer-ig-story": "https://shopee.tw/product/474186812/25292961904/",
+
+  // Email
+  "flstudiohiphopproducer-email": "https://shopee.tw/product/474186812/25292961904/",
+
+  // Website
+  "flstudiohiphopproducer-site": "https://shopee.tw/product/474186812/25292961904/",
+
+  // Threads
+  "flstudiohiphopproducer-threads": "https://shopee.tw/product/474186812/25292961904/",
+
+  // =====================
+// 歸剛 Vibe Coding
+// =====================
+
+"vibecoding": "https://shopee.tw/product/474186812/41956667281/",
+
+// YouTube
+"vibecoding-yt": "https://shopee.tw/product/474186812/41956667281/",
+"vibecoding-yts": "https://shopee.tw/product/474186812/41956667281/",
+
+// Instagram
+"vibecoding-ig-bio": "https://shopee.tw/product/474186812/41956667281/",
+"vibecoding-ig-story": "https://shopee.tw/product/474186812/41956667281/",
+
+// Email
+"vibecoding-email": "https://shopee.tw/product/474186812/41956667281/",
+
+// Website
+"vibecoding-site": "https://shopee.tw/product/474186812/41956667281/",
+
+// Threads
+"vibecoding-threads": "https://shopee.tw/product/474186812/41956667281/",
+
+  // =====================
+  // 和弦故事
+  // =====================
+
+  "chordstory": "https://shopee.tw/product/474186812/27520833699/",
+
+  // YouTube
+  "chordstory-yt": "https://shopee.tw/product/474186812/27520833699/",
+  "chordstory-yts": "https://shopee.tw/product/474186812/27520833699/",
+
+  // Instagram
+  "chordstory-ig-bio": "https://shopee.tw/product/474186812/27520833699/",
+  "chordstory-ig-story": "https://shopee.tw/product/474186812/27520833699/",
+
+  // Email
+  "chordstory-email": "https://shopee.tw/product/474186812/27520833699/",
+
+  // Website
+  "chordstory-site": "https://shopee.tw/product/474186812/27520833699/",
+
+  // Threads
+  "chordstory-threads": "https://shopee.tw/product/474186812/27520833699/",
+
+
+  // =====================
+  // 聲活
+  // =====================
+
+  "asoundlife": "https://shopee.tw/product/474186812/26319762261/",
+
+  // YouTube
+  "asoundlife-yt": "https://shopee.tw/product/474186812/26319762261/",
+  "asoundlife-yts": "https://shopee.tw/product/474186812/26319762261/",
+
+  // Instagram
+  "asoundlife-ig-bio": "https://shopee.tw/product/474186812/26319762261/",
+  "asoundlife-ig-story": "https://shopee.tw/product/474186812/26319762261/",
+
+  // Email
+  "asoundlife-email": "https://shopee.tw/product/474186812/26319762261/",
+
+  // Website
+  "asoundlife-site": "https://shopee.tw/product/474186812/26319762261/",
+
+  // Threads
+  "asoundlife-threads": "https://shopee.tw/product/474186812/26319762261/",
+
+
+  // =====================
+  // 耳朵之間
+  // =====================
+
+  "betweentheears": "https://shopee.tw/product/474186812/41801886381/",
+
+  // YouTube
+  "betweentheears-yt": "https://shopee.tw/product/474186812/41801886381/",
+  "betweentheears-yts": "https://shopee.tw/product/474186812/41801886381/",
+
+  // Instagram
+  "betweentheears-ig-bio": "https://shopee.tw/product/474186812/41801886381/",
+  "betweentheears-ig-story": "https://shopee.tw/product/474186812/41801886381/",
+
+  // Email
+  "betweentheears-email": "https://shopee.tw/product/474186812/41801886381/",
+
+  // Website
+  "betweentheears-site": "https://shopee.tw/product/474186812/41801886381/",
+
+  // Threads
+  "betweentheears-threads": "https://shopee.tw/product/474186812/41801886381/",
+
+
+  // =====================
+  // 毛孩女戰神
+  // =====================
+
+  "petfighter": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
+
+  // YouTube
+  "petfighter-yt": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
+  "petfighter-yts": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
+
+  // Instagram
+  "petfighter-ig-bio": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
+  "petfighter-ig-story": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
+
+  // Email
+  "petfighter-email": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
+
+  // Website
+  "petfighter-site": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
+
+  // Threads
+  "petfighter-threads": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
+};  
   export default LINKS;
