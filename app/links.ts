e03f59681761285269bbbde5062bc300bc63bf18
Peiju Lin <linkevin422@gmail.com>
2026-04-27 19:12:52 +0800

    changed links

--- app/links.ts
+++ app/links.ts
@@ -9,209 +9,209 @@ const LINKS: Record<string, string> = {
   // 連結設計
   // =====================
 
-  "thinknode": "https://shopee.tw/product/474186812/47608007722/",
+  "thinknode": "https://peijulink.com/books/thinkinnodes",
 
   // YouTube
-  "thinknode-yt": "https://shopee.tw/product/474186812/47608007722/",
-  "thinknode-yts": "https://shopee.tw/product/474186812/47608007722/",
+  "thinknode-yt": "https://peijulink.com/books/thinkinnodes",
+  "thinknode-yts": "https://peijulink.com/books/thinkinnodes",
 
   // Instagram
-  "thinknode-ig-bio": "https://shopee.tw/product/474186812/47608007722/",
-  "thinknode-ig-story": "https://shopee.tw/product/474186812/47608007722/",
+  "thinknode-ig-bio": "https://peijulink.com/books/thinkinnodes",
+  "thinknode-ig-story": "https://peijulink.com/books/thinkinnodes",
 
   // Email
-  "thinknode-email": "https://shopee.tw/product/474186812/47608007722/",
+  "thinknode-email": "https://peijulink.com/books/thinkinnodes",
 
   // Website
   "thinknode-site": "https://shopee.tw/product/474186812/47608007722/",
 
   // Threads
-  "thinknode-threads": "https://shopee.tw/product/474186812/47608007722/",
+  "thinknode-threads": "https://peijulink.com/books/thinkinnodes",
 
   //TT
-  "thinknode-tt": "https://shopee.tw/product/474186812/47608007722/",
+  "thinknode-tt": "https://peijulink.com/books/thinkinnodes",
 
   // =====================
   // 音樂製作人 X FL Studio
   // =====================
 
-  "flstudioproducer": "https://shopee.tw/product/474186812/27465576743/",
+  "flstudioproducer": "https://peijulink.com/books/flstudioproducer",
 
   // YouTube
-  "flstudioproducer-yt": "https://shopee.tw/product/474186812/27465576743/",
-  "flstudioproducer-yts": "https://shopee.tw/product/474186812/27465576743/",
+  "flstudioproducer-yt": "https://peijulink.com/books/flstudioproducer",
+  "flstudioproducer-yts": "https://peijulink.com/books/flstudioproducer",
 
   // Instagram
-  "flstudioproducer-ig-bio": "https://shopee.tw/product/474186812/27465576743/",
-  "flstudioproducer-ig-story": "https://shopee.tw/product/474186812/27465576743/",
+  "flstudioproducer-ig-bio": "https://peijulink.com/books/flstudioproducer",
+  "flstudioproducer-ig-story": "https://peijulink.com/books/flstudioproducer",
 
   // Email
-  "flstudioproducer-email": "https://shopee.tw/product/474186812/27465576743/",
+  "flstudioproducer-email": "https://peijulink.com/books/flstudioproducer",
 
   // Website
   "flstudioproducer-site": "https://shopee.tw/product/474186812/27465576743/",
 
   // Threads
-  "flstudioproducer-threads": "https://shopee.tw/product/474186812/27465576743/",
+  "flstudioproducer-threads": "https://peijulink.com/books/flstudioproducer",
 
   //TT
-  "flstudioproducer-tt": "https://shopee.tw/product/474186812/27465576743/",
+  "flstudioproducer-tt": "https://peijulink.com/books/flstudioproducer",
 
   // =====================
   // FL Studio 嘻哈製作人
   // =====================
 
-  "flstudiohiphopproducer": "https://shopee.tw/product/474186812/25292961904/",
+  "flstudiohiphopproducer": "https://peijulink.com/books/flstudiohiphopproducer",
 
   // YouTube
-  "flstudiohiphopproducer-yt": "https://shopee.tw/product/474186812/25292961904/",
-  "flstudiohiphopproducer-yts": "https://shopee.tw/product/474186812/25292961904/",
+  "flstudiohiphopproducer-yt": "https://peijulink.com/books/flstudiohiphopproducer",
+  "flstudiohiphopproducer-yts": "https://peijulink.com/books/flstudiohiphopproducer",
 
   // Instagram
-  "flstudiohiphopproducer-ig-bio": "https://shopee.tw/product/474186812/25292961904/",
-  "flstudiohiphopproducer-ig-story": "https://shopee.tw/product/474186812/25292961904/",
+  "flstudiohiphopproducer-ig-bio": "https://peijulink.com/books/flstudiohiphopproducer",
+  "flstudiohiphopproducer-ig-story": "https://peijulink.com/books/flstudiohiphopproducer",
 
   // Email
-  "flstudiohiphopproducer-email": "https://shopee.tw/product/474186812/25292961904/",
+  "flstudiohiphopproducer-email": "https://peijulink.com/books/flstudiohiphopproducer",
 
   // Website
   "flstudiohiphopproducer-site": "https://shopee.tw/product/474186812/25292961904/",
 
   // Threads
-  "flstudiohiphopproducer-threads": "https://shopee.tw/product/474186812/25292961904/",
+  "flstudiohiphopproducer-threads": "https://peijulink.com/books/flstudiohiphopproducer",
 
   //TT
-  "flstudiohiphopproducer-tt": "https://shopee.tw/product/474186812/25292961904/",
+  "flstudiohiphopproducer-tt": "https://peijulink.com/books/flstudiohiphopproducer",
 
   // =====================
 // 歸剛 Vibe Coding
 // =====================
 
-"vibecoding": "https://shopee.tw/product/474186812/41956667281/",
+"vibecoding": "https://peijulink.com/books/vibecoding",
 
 // YouTube
-"vibecoding-yt": "https://shopee.tw/product/474186812/41956667281/",
-"vibecoding-yts": "https://shopee.tw/product/474186812/41956667281/",
+"vibecoding-yt": "https://peijulink.com/books/vibecoding",
+"vibecoding-yts": "https://peijulink.com/books/vibecoding",
 
 // Instagram
-"vibecoding-ig-bio": "https://shopee.tw/product/474186812/41956667281/",
-"vibecoding-ig-story": "https://shopee.tw/product/474186812/41956667281/",
+"vibecoding-ig-bio": "https://peijulink.com/books/vibecoding",
+"vibecoding-ig-story": "https://peijulink.com/books/vibecoding",
 
 // Email
-"vibecoding-email": "https://shopee.tw/product/474186812/41956667281/",
+"vibecoding-email": "https://peijulink.com/books/vibecoding",
 
 // Website
 "vibecoding-site": "https://shopee.tw/product/474186812/41956667281/",
 
 // Threads
-"vibecoding-threads": "https://shopee.tw/product/474186812/41956667281/",
+"vibecoding-threads": "https://peijulink.com/books/vibecoding",
 
 //TT
-"vibecoding-tt": "https://shopee.tw/product/474186812/41956667281/",
+"vibecoding-tt": "https://peijulink.com/books/vibecoding",
 
   // =====================
   // 和弦故事
   // =====================
 
-  "chordstory": "https://shopee.tw/product/474186812/27520833699/",
+  "chordstory": "https://peijulink.com/books/chordstory",
 
   // YouTube
-  "chordstory-yt": "https://shopee.tw/product/474186812/27520833699/",
-  "chordstory-yts": "https://shopee.tw/product/474186812/27520833699/",
+  "chordstory-yt": "https://peijulink.com/books/chordstory",
+  "chordstory-yts": "https://peijulink.com/books/chordstory",
 
   // Instagram
-  "chordstory-ig-bio": "https://shopee.tw/product/474186812/27520833699/",
-  "chordstory-ig-story": "https://shopee.tw/product/474186812/27520833699/",
+  "chordstory-ig-bio": "https://peijulink.com/books/chordstory",
+  "chordstory-ig-story": "https://peijulink.com/books/chordstory",
 
   // Email
-  "chordstory-email": "https://shopee.tw/product/474186812/27520833699/",
+  "chordstory-email": "https://peijulink.com/books/chordstory",
 
   // Website
   "chordstory-site": "https://shopee.tw/product/474186812/27520833699/",
 
   // Threads
-  "chordstory-threads": "https://shopee.tw/product/474186812/27520833699/",
+  "chordstory-threads": "https://peijulink.com/books/chordstory",
 
   //TT
-  "chordstory-tt": "https://shopee.tw/product/474186812/27520833699/",
+  "chordstory-tt": "https://peijulink.com/books/chordstory",
 
 
   // =====================
   // 聲活
   // =====================
 
-  "asoundlife": "https://shopee.tw/product/474186812/26319762261/",
+  "asoundlife": "https://peijulink.com/books/asoundlife",
 
   // YouTube
-  "asoundlife-yt": "https://shopee.tw/product/474186812/26319762261/",
-  "asoundlife-yts": "https://shopee.tw/product/474186812/26319762261/",
+  "asoundlife-yt": "https://peijulink.com/books/asoundlife",
+  "asoundlife-yts": "https://peijulink.com/books/asoundlife",
 
   // Instagram
-  "asoundlife-ig-bio": "https://shopee.tw/product/474186812/26319762261/",
-  "asoundlife-ig-story": "https://shopee.tw/product/474186812/26319762261/",
+  "asoundlife-ig-bio": "https://peijulink.com/books/asoundlife",
+  "asoundlife-ig-story": "https://peijulink.com/books/asoundlife",
 
   // Email
-  "asoundlife-email": "https://shopee.tw/product/474186812/26319762261/",
+  "asoundlife-email": "https://peijulink.com/books/asoundlife",
 
   // Website
   "asoundlife-site": "https://shopee.tw/product/474186812/26319762261/",
 
   // Threads
-  "asoundlife-threads": "https://shopee.tw/product/474186812/26319762261/",
+  "asoundlife-threads": "https://peijulink.com/books/asoundlife",
 
   //TT
-  "asoundlife-tt": "https://shopee.tw/product/474186812/26319762261/",
+  "asoundlife-tt": "https://peijulink.com/books/asoundlife",
 
   // =====================
   // 耳朵之間
   // =====================
 
-  "betweentheears": "https://shopee.tw/product/474186812/41801886381/",
+  "betweentheears": "https://peijulink.com/books/betweentheears",
 
   // YouTube
-  "betweentheears-yt": "https://shopee.tw/product/474186812/41801886381/",
-  "betweentheears-yts": "https://shopee.tw/product/474186812/41801886381/",
+  "betweentheears-yt": "https://peijulink.com/books/betweentheears",
+  "betweentheears-yts": "https://peijulink.com/books/betweentheears",
 
   // Instagram
-  "betweentheears-ig-bio": "https://shopee.tw/product/474186812/41801886381/",
-  "betweentheears-ig-story": "https://shopee.tw/product/474186812/41801886381/",
+  "betweentheears-ig-bio": "https://peijulink.com/books/betweentheears",
+  "betweentheears-ig-story": "https://peijulink.com/books/betweentheears",
 
   // Email
-  "betweentheears-email": "https://shopee.tw/product/474186812/41801886381/",
+  "betweentheears-email": "https://peijulink.com/books/betweentheears",
 
   // Website
   "betweentheears-site": "https://shopee.tw/product/474186812/41801886381/",
 
   // Threads
-  "betweentheears-threads": "https://shopee.tw/product/474186812/41801886381/",
+  "betweentheears-threads": "https://peijulink.com/books/betweentheears",
 
   //TT
-  "betweentheears-tt": "https://shopee.tw/product/474186812/41801886381/",
+  "betweentheears-tt": "https://peijulink.com/books/betweentheears",
 
   // =====================
   // 毛孩女戰神
   // =====================
 
-  "petfighter": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
+  "petfighter": "https://peijulink.com/books/petfighter",
 
   // YouTube
-  "petfighter-yt": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
-  "petfighter-yts": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
+  "petfighter-yt": "https://peijulink.com/books/petfighter",
+  "petfighter-yts": "https://peijulink.com/books/petfighter",
 
   // Instagram
-  "petfighter-ig-bio": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
-  "petfighter-ig-story": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
+  "petfighter-ig-bio": "https://peijulink.com/books/petfighter",
+  "petfighter-ig-story": "https://peijulink.com/books/petfighter",
 
   // Email
-  "petfighter-email": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
+  "petfighter-email": "https://peijulink.com/books/petfighter",
 
   // Website
   "petfighter-site": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
 
   //TT
-  "petfighter-tt": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
+  "petfighter-tt": "https://peijulink.com/books/petfighter",
   
   // Threads
-  "petfighter-threads": "https://shopee.tw/%E6%AF%9B%E5%AD%A9%E5%A5%B3%E6%88%B0%E7%A5%9E-%E6%AF%9B%E5%AD%A9%E5%AE%88%E8%AD%B7%E8%80%85-2025-%E7%8B%97%E7%8B%97%E5%8E%BB%E7%BE%8E%E5%9C%8B-%E7%8B%97%E7%8B%97%E5%8E%BB%E9%9F%93%E5%9C%8B-%E5%8F%B0%E7%81%A3%E5%85%A5%E5%A2%83%E5%85%8D%E9%9A%94%E9%9B%A2%E6%B5%81%E7%A8%8B-%E7%8B%97%E7%8B%97%E8%B2%93%E8%B2%93%E6%98%AF%E5%AE%B6%E4%BA%BA-i.1433610645.24789887570",
+  "petfighter-threads": "https://peijulink.com/books/petfighter",
 };  
   export default LINKS;
